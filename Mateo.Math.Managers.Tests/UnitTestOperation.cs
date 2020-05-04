28ef7c6cf8462d2041fc8fd6702f3b4cae70390f
Anthony COUDENE <anthony.coudene@technidata-web.com>
2020-05-04 11:20:53 +0200

    Change look&feel, add new functionalities

--- Mateo.Math.Managers.Tests/UnitTestOperation.cs
+++ Mateo.Math.Managers.Tests/UnitTestOperation.cs
@@ -7,22 +7,29 @@ namespace Mateo.Math.Managers.Tests
 {
   [TestClass]
   public class UnitTestOperation
-  {   
-    public void TestMethodExerciseFor<O>(int mandatoryValue, int nbTests = 100) 
+  {
+    public void TestMethodExerciseFor<O>(
+      int mandatoryValue,
+      Func<O, bool> funCheck = null,
+      int limitMinOtherValue = 1, 
+      int limitMaxOtherValue = 101,
+      bool? forceMandatoryPositionRight = null,
+      int nbTests = 100) 
       where O : IBinaryOperation<int>, new()
     {      
       for (int i = 0; i < nbTests; i++)
       {
-        var formula = ExerciseOnOperation.GenerateFormulaForMandatoryOperand<O>(mandatoryValue);        
+        var formula = ExerciseOnOperation.GenerateFormulaForMandatoryOperand<O>(mandatoryValue, limitMinOtherValue, limitMaxOtherValue, forceMandatoryPositionRight);        
         Console.WriteLine($"Formula (mandatory operand={mandatoryValue}): {formula.LeftOperand} {formula.OperatorSymbol} {formula.RightOperand}");
         Console.WriteLine($"Result={formula.DoOperation()}");
+        Assert.IsTrue(funCheck == null || funCheck(formula), "This formula does not respect the constraints!");
       }
     }
 
     [TestMethod]
     public void TestMethodAddExerciseFor()
     {
-      var iterations = Gen.Sample(0, 1000, Gen.Choose(0, 10));
+      var iterations = Gen.Sample(0, 1000, Gen.Choose(1, 9));
       foreach (var iteration in iterations)
       {
         Console.WriteLine($"Test exercise for {iteration}: ");
@@ -33,11 +40,11 @@ namespace Mateo.Math.Managers.Tests
     [TestMethod]
     public void TestMethodSubExerciseFor()
     {
-      var iterations = Gen.Sample(0, 1000, Gen.Choose(0, 10));
+      var iterations = Gen.Sample(0, 1000, Gen.Choose(1, 9));
       foreach (var iteration in iterations)
       {
         Console.WriteLine($"Test exercise for {iteration}: ");
-        TestMethodExerciseFor<SubOperation<int>>(iteration);
+        TestMethodExerciseFor<SubOperation<int>>(iteration, o => o.DoOperation() >= 0, iteration, forceMandatoryPositionRight:true);
       }
     }
 
--- Mateo.Math.Managers/ExerciseOnOperation.cs
+++ Mateo.Math.Managers/ExerciseOnOperation.cs
@@ -6,22 +6,22 @@ namespace Mateo.Math.Managers
 {
   public static class ExerciseOnOperation
   {    
-    public static O GenerateFormulaForMandatoryOperand<O>(int mandatoryOperand, int limitOtherValue = 101, bool? ForceMandatoryPositionLeft = null)
+    public static O GenerateFormulaForMandatoryOperand<O>(int mandatoryOperand, int limitMinOtherValue = 1, int limitMaxOtherValue = 101, bool? forceMandatoryPositionRight = null)
       where O : IBinaryOperation<int>, new()
     {
-      var otherOperand = new System.Random().Next(0, limitOtherValue);      
-      var IsLeftOperandMandatory = (ForceMandatoryPositionLeft.HasValue) ? (ForceMandatoryPositionLeft.Value) : new Random().Next(2) == 1;     
+      var otherOperand = new System.Random().Next(limitMinOtherValue, limitMaxOtherValue);      
+      var IsRightOperandMandatory = (forceMandatoryPositionRight.HasValue) ? (forceMandatoryPositionRight.Value) : new Random().Next(2) == 1;
 
-      return (IsLeftOperandMandatory)
-        ? new O() { LeftOperand = new Operand<int>(mandatoryOperand), RightOperand = new Operand<int>(otherOperand) } 
-        : new O() { LeftOperand = new Operand<int>(otherOperand), RightOperand = new Operand<int>(mandatoryOperand) };
+      return (IsRightOperandMandatory)
+        ? new O() { LeftOperand = new Operand<int>(otherOperand), RightOperand = new Operand<int>(mandatoryOperand) }
+        : new O() { LeftOperand = new Operand<int>(mandatoryOperand), RightOperand = new Operand<int>(otherOperand) }; 
     }
 
-    public static O GenerateFormula<O>(int limitOperandValue = 101)
+    public static O GenerateFormula<O>(int limitMinOtherValue = 1, int limitMaxOtherValue = 101)
       where O : IBinaryOperation<int>, new()
     {
-      var leftOperand = new System.Random().Next(0, limitOperandValue);
-      var rightOperand = new System.Random().Next(0, limitOperandValue);
+      var leftOperand = new System.Random().Next(limitMinOtherValue, limitMaxOtherValue);
+      var rightOperand = new System.Random().Next(limitMinOtherValue, limitMaxOtherValue);
 
       return new O() { LeftOperand = new Operand<int>(leftOperand), RightOperand = new Operand<int>(rightOperand) };
     }
